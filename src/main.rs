9dc497751a48679a3ce624188469b255ffd6160b
Vincent Ollivier <v@vinc.cc>
2016-05-07 03:55:12 +0000

    Add print ephemeris option

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,14 @@ use std::env;
 
 fn main() {
     let mut use_solar_calendar = false;
+    let mut print_ephemeris = false;
 
     let args: Vec<_> = env::args().filter(|arg| {
         if arg == "--solar" {
             use_solar_calendar = true
+        }
+        if arg == "--ephem" {
+            print_ephemeris = true
         }
 
         !arg.starts_with("--")
@@ -44,21 +48,23 @@ fn main() {
     let lat = args[1].parse::<f64>().unwrap();
     let lon = args[2].parse::<f64>().unwrap();
 
-    //println!("{}", get_date(now, lon, use_solar_calendar));
+    if print_ephemeris {
+        let mut events = BTreeMap::new();
 
-    let mut events = BTreeMap::new();
+        events.insert(now, "Current: ");
 
-    events.insert(now, "Current: ");
+        if let Some(sunrise) = get_sunrise(now, lon, lat) {
+            events.insert(sunrise, "Sunrise: ");
+        }
 
-    if let Some(sunrise) = get_sunrise(now, lon, lat) {
-        events.insert(sunrise, "Sunrise: ");
-    }
+        if let Some(sunset) = get_sunset(now, lon, lat) {
+            events.insert(sunset, "Sunset:  ");
+        }
 
-    if let Some(sunset) = get_sunset(now, lon, lat) {
-        events.insert(sunset, "Sunset:  ");
-    }
-
-    for (&time, name) in &events {
-        println!("{} {}", name, get_date(time, lon, use_solar_calendar));
+        for (&time, name) in &events {
+            println!("{} {}", name, get_date(time, lon, use_solar_calendar));
+        }
+    } else {
+        println!("{}", get_date(now, lon, use_solar_calendar));
     }
 }
